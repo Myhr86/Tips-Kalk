7dbeb5a90b926685ac51fcb44dd82f6f6ea23659
Myhr86 <stein_myhr@hotmail.com>
2019-03-02 17:37:39 +0100

    Update script.js
    Added if else statements, that differentiates between 1 or more persons sharing the bill

--- www/js/script.js
+++ www/js/script.js
@@ -8,12 +8,24 @@ function calculateTip() {
 
   if(service === 'bra') {
     var braSum = sumPerPerson * 20 / (100);
+    if(persons == 1) {
     document.getElementById('output').innerHTML = "Du kan tipse " + braSum + " kr";
+  } else {
+    document.getElementById('output').innerHTML = "Dere kan tipse " + braSum + " kr hver";
+  }
   } else if(service === 'middels') {
     var middelSum = sumPerPerson * 10 / (100);
-    document.getElementById('output').innerHTML = "Du kan tipse" + middelSum + " kr";
+    if(persons == 1) {
+    document.getElementById('output').innerHTML = "Du kan tipse " + middelSum + " kr";
+  } else {
+    document.getElementById('output').innerHTML = "Dere kan tipse " + middelSum + " kr hver";
+  }
   } else {
     var dårligSum = sumPerPerson * 5 / (100);
+    if(persons == 1) {
     document.getElementById('output').innerHTML = "Du kan tipse " + dårligSum + " kr";
+  } else {
+    document.getElementById('output').innerHTML = "Dere kan tipse " + dårligSum + " kr hver";
+  }
   }
 }
